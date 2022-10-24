e8f027ab911c43462a117d1b4c0596cb5c6b55af
Alex Kochutov <alex.kochutov@gmail.com>
2022-10-24 23:17:22 +0400

    feat: add Film class file

---
+++ src/main/java/ru/netology/Film.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+public class Film {
+    public int id;
+    public String title;
+    public String genre;
+
+    public Film(int id, String title, String genre) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+    }
+}
